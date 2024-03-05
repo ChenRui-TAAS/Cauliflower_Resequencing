bb0015db7d1656dc72c99c076c49c12570c6ce59
ChenRui-TAAS <chenrui.taas@gmail.com>
2024-03-05 21:44:21 +0800

    Delete 5_Identification_of_differentiated_regions directory

--- 5_Identification_of_differentiated_regions/fst.sh
+++
@@ -1,8 +0,0 @@
-perl bin/slipping.pl Chr${i} length_of_Chr${i}        Group1 Group2 0 100000
-
-python3 abstract_SlipingFst.py -i ../Group2_Group1/Chr${i}.fst.overall -chr Chr${i} -o ./Chr${i}.fst.overall_final
-
-###then select top5 fst
-
-###construct sweep regions
-count_top_percent_5_divergence_region.py -i top_5_chr_pos_sort.fst -o sweep_regions.txt
